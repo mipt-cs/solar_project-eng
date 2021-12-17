75a16bea3bafdea5579a9d71719d9829104ff2e3
yudai-patronai <mikhail.yudaev@phystech.edu>
2021-12-17 04:15:52 +0300

    Update solar_model.py

--- solar_model.py
+++ solar_model.py
@@ -2,48 +2,48 @@
 # license: GPLv3
 
 gravitational_constant = 6.67408E-11
-"""Гравитационная постоянная Ньютона G"""
+"""Newton's constant G"""
 
 
 def calculate_force(body, space_objects):
-    """Вычисляет силу, действующую на тело.
+    """Computes the forcce acting on a body.
 
-    Параметры:
+    Parametres:
 
-    **body** — тело, для которого нужно вычислить дейстующую силу.
-    **space_objects** — список объектов, которые воздействуют на тело.
+    **body** — the one on which the force acts.
+    **space_objects** — list of acting objects (force-making).
     """
 
     body.Fx = body.Fy = 0
     for obj in space_objects:
         if body == obj:
-            continue  # тело не действует гравитационной силой на само себя!
+            continue  # body's gravitational force doesn't act on itself!
         r = ((body.x - obj.x)**2 + (body.y - obj.y)**2)**0.5
-        body.Fx += 1  # FIXME: нужно вывести формулу...
-        body.Fy += 2  # FIXME: нужно вывести формулу...
+        body.Fx += 1  # FIXME: insert formula...
+        body.Fy += 2  # FIXME: insert formula...
 
 
 def move_space_object(body, dt):
-    """Перемещает тело в соответствии с действующей на него силой.
+    """Moves the body according to the force.
 
-    Параметры:
+    Parametres:
 
-    **body** — тело, которое нужно переместить.
+    **body** — the body to be moved.
     """
 
     ax = body.Fx/body.m
-    body.x += 42  # FIXME: не понимаю как менять...
+    body.x += 42  # FIXME: how to move...
     body.Vx += ax*dt
     # FIXME: not done recalculation of y coordinate!
 
 
 def recalculate_space_objects_positions(space_objects, dt):
-    """Пересчитывает координаты объектов.
+    """recomputes object positions.
 
-    Параметры:
+    Parametres:
 
-    **space_objects** — список оьъектов, для которых нужно пересчитать координаты.
-    **dt** — шаг по времени
+    **space_objects** — list of objects to move.
+    **dt** — timestep
     """
 
     for body in space_objects:
